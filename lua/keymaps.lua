cdd884ddb4d000cfd91c8d33192b6ee641cbe18b
burnic <espelandvidar@gmail.com>
2024-10-13 00:49:20 +0200

    update keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -71,5 +71,5 @@ vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
 vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
 vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
 vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
-vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set('n', '<leader>S', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 -- vim: ts=2 sts=2 sw=2 et
